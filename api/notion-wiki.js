1b5a71b5690e648ae928a30b52ffe7c753c97047
Marcel Zöller <marcelzoeller@gmail.com>
2025-05-02 22:11:18 +0200

    Fix page retrieval method in Notion API handler to use blocks.children.list

--- api/notion-wiki.js
+++ api/notion-wiki.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const { pageId } = req.query;
 
   try {
-    const page = await notion.pages.retrieve({ page_id: pageId });
+    const page = await notion.blocks.children.list({ block_id: pageId });
     res.status(200).json(page);
   } catch (err) {
     res.status(500).json({ error: err.message });
