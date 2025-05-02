59156a3bf9319033cbfeb328d6d8ba8c9d14b965
Marcel Zöller <marcelzoeller@gmail.com>
2025-05-02 18:18:57 +0200

    init

---
+++ api/wiki.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+    const file = req.query.file || "index.md";
+    const githubUser = "DEIN_USERNAME";
+    const repo = "wiki-data";
+    const branch = "main";
+  
+    const url = `https://raw.githubusercontent.com/${githubUser}/${repo}/${branch}/wiki/${file}`;
+  
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+    });
+  
+    if (!response.ok) {
+      return res.status(404).send("Datei nicht gefunden");
+    }
+  
+    const content = await response.text();
+    res.status(200).send(content);
+  }
+  
