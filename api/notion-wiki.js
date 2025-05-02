827aab44e90016f75f49468af28543003928209b
Marcel Zöller <marcelzoeller@gmail.com>
2025-05-02 19:33:00 +0200

    Refactor API handlers to streamline authorization checks and improve security

---
+++ api/notion-wiki.js
@@ -0,0 +1,14 @@
+import { Client } from "@notionhq/client";
+
+const notion = new Client({ auth: process.env.NOTION_TOKEN });
+
+export default async function handler(req, res) {
+  const { pageId } = req.query;
+
+  try {
+    const page = await notion.pages.retrieve({ page_id: pageId });
+    res.status(200).json(page);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
