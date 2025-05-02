d9db13ecb54b8b30108c914164cb1b383d06bd0d
Marcel Zöller <marcelzoeller@gmail.com>
2025-05-02 22:00:17 +0200

    Fix parent property assignment in Notion API handler for accurate database ID comparison

--- api/notion-list.js
+++ api/notion-list.js
@@ -22,7 +22,9 @@ export default async function handler(req, res) {
 
       let parent = null;
 
-      if (page.parent?.database_id && page.parent.database_id == databaseId) {
+      if (page.parent?.database_id && page.parent.database_id === databaseId) {
+        parent = null
+      } else {
         parent = page.parent;
       }
 
