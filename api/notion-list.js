572d80299b7af44f9eae86c07ab29494cfdb92a6
Marcel Zöller <marcelzoeller@gmail.com>
2025-05-02 21:24:35 +0200

    Fix title extraction in Notion API handler to correctly return page title

--- api/notion-list.js
+++ api/notion-list.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
 
   
     const titles = response.results.map((page) => {
-      return page.id
+      return page.properties.Page.id
       /*const titleProperty = page.properties;
       if (!titleProperty || !titleProperty.Page.title.length <= 0 || !titleProperty.Page.title[0]?.text?.content) {
         return "Unbenannt";
